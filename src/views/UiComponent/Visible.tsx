5e915fe68e1c9b3e6588ceb5a3145403b9b60e27
春雷-李春雷 <chunlei.lcl@naturobot.com>
2022-10-25 14:13:32 +0800

    r

--- src/views/UiComponent/Visible.tsx
+++ src/views/UiComponent/Visible.tsx
@@ -3,11 +3,18 @@ import React, { useEffect, useRef, useState } from 'react'
 
 const Visible: React.FC = props => {
   const [bool, setBool] = useState(false)
+  const [value, setValue] = useState('false')
+
+  const C = props => {
+    return <input value={props.value} onChange={props.onChange} />
+  }
 
   return (
     <div className="v-v">
       <Button onClick={() => setBool(!bool)}>setBool {String(bool)}</Button>
       <ViewVisible visible={bool}>哈哈哈</ViewVisible>
+
+      <C key="a" value={value} onChange={evt => setValue(evt.target.value)} />
 
       <hr />
     </div>
