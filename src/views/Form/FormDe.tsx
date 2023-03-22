29008d48ad858442f4c2c057328f21d18accbeab
春雷-李春雷 <chunlei.lcl@naturobot.com>
2023-03-22 22:09:55 +0800

    arco form

--- src/views/Form/FormDe.tsx
+++ src/views/Form/FormDe.tsx
@@ -1,203 +1,64 @@
-import React, { useContext, useRef, useState } from 'react'
-import { Button, Checkbox, Input } from 'antd'
+import { Form, Input, Button, InputNumber } from '@arco-design/web-react'
+const FormItem = Form.Item
 
-type FormProps = {
-  children: React.ReactNode | React.ReactElement[]
-  name?: string
-  initialValues?: any
-  onFinish?: (values) => void
-  onFinishFailed?: (errorInfo) => void
-}
-
-type IFormItemProps = {
-  children: React.ReactElement
-  label?: React.ReactNode
-  name?: string
-  rules?: any[]
-  valuePropName?: string
-
-  errMsg?: string
-}
-type IForm = React.FC<FormProps> & { Item: React.FC<IFormItemProps> }
-
-const FormContext = React.createContext<
-  FormProps & {
-    valuesRef: React.RefObject<any>
-    handleFinish: () => void
-    validateItemField: (name: string, _val: any) => void
-  }
->(undefined)
-
-const validate = (_val, rules) => {
-  for (const ruleItem of rules) {
-    if (ruleItem.required && !_val) {
-      return {
-        isValidate: false,
-        errMsg: ruleItem.message
-      }
-    }
-  }
-
-  return { isValidate: true, errMsg: '' }
-}
-
-const Form: IForm = props => {
-  const { children, name, initialValues, onFinish, onFinishFailed } = props
-
-  const valuesRef = useRef(initialValues || {})
-
-  const [errMsg, setErrMsg] = useState({})
-
-  const childrenArray = React.Children.toArray(children) as React.ReactElement[]
-
-  const validateItemField = (name: string, _val: any) => {
-    const rules = childrenArray.find(item => item.props.name === name).props.rules
-
-    if (!rules) {
-      setErrMsg({ ...errMsg, [name]: '' })
-      return { isValidate: true, errMsg: '' }
-    }
-
-    const validateRes = validate(_val, rules)
-
-    setErrMsg({ ...errMsg, [name]: validateRes.isValidate ? '' : validateRes.errMsg })
-
-    return validateRes
-  }
-
-  const validateAllField = () => {
-    const validateFailedResult = []
-    for (const childrenItem of childrenArray) {
-      const { name } = childrenItem.props
-      const _val = valuesRef.current[name]
-
-      const { isValidate, errMsg } = validateItemField(name, _val)
-
-      if (!isValidate) {
-        validateFailedResult.push({ name, errMsg })
-      }
-    }
-
-    return validateFailedResult
-  }
-
-  const handleFinish = () => {
-    const errArray = validateAllField()
-
-    if (errArray.length) {
-      onFinishFailed(errArray)
-
-      setErrMsg(errArray.reduce((acc, item) => Object.assign(acc, { [item.name]: item.errMsg }), {}))
-      return
-    }
-
-    onFinish(valuesRef.current)
-  }
-
-  const childrenJsx = childrenArray.map(item => {
-    const isFormItemComponent = item.type === Form.Item
-
-    if (!isFormItemComponent) return item
-
-    return {
-      ...item,
-      props: {
-        ...item.props,
-        errMsg: errMsg[item.props.name]
-      }
-    }
-  })
+function App() {
+  const [form] = Form.useForm()
 
   return (
-    <FormContext.Provider value={{ ...props, valuesRef, handleFinish, validateItemField }}>
-      {childrenJsx}
-    </FormContext.Provider>
-  )
-}
-
-Form.Item = props => {
-  const { label, name, rules, valuePropName, children, errMsg } = props
-
-  const context = useContext(FormContext)
-
-  const initialValue = context.initialValues[name]
-
-  const defaultValKey = valuePropName
-    ? 'default' + valuePropName.replace(/^./, firstChar => firstChar.toUpperCase()) // 将第一个首字母大写
-    : 'defaultValue'
-
-  const valKey = valuePropName ? valuePropName : 'value'
-
-  const ChildElement = React.cloneElement(children, {
-    [defaultValKey]: initialValue,
-    onChange: evt => {
-      const _val = evt.target[valKey]
-
-      context.valuesRef.current[name] = _val
-
-      context.validateItemField(name, _val)
-    },
-    onClick: () => {
-      if (children.props.htmlType === 'submit') {
-        context.handleFinish()
-      }
-    }
-  })
-
-  const hasRequired = rules ? [].concat(rules).some(rule => rule.required) : false
-
-  return (
-    <div style={{ display: 'flex' }}>
-      <div style={{ width: 100 }}>
-        {hasRequired && <span style={{ color: 'red' }}>*</span>}
-        {label}
-      </div>
-
-      <div>
-        {ChildElement}
-        {errMsg && <div style={{ color: 'red' }}>{errMsg}</div>}
-      </div>
-    </div>
-  )
-}
-
-const FormDe: React.FC = () => {
-  const onFinish = (values: any) => {
-    console.log('Success:', values)
-  }
-
-  const onFinishFailed = (errorInfo: any) => {
-    console.log('Failed:', errorInfo)
-  }
-
-  return (
-    <Form name="basic" initialValues={{ remember: true }} onFinish={onFinish} onFinishFailed={onFinishFailed}>
-      <h4 style={{ fontSize: 18 }}>我不是Form.Item </h4>
-
-      <Form.Item
-        label="用户名"
-        name="username"
-        rules={[{ required: true, message: 'Please input your username!' }]}
-      >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        label="密码"
-        name="password"
-        rules={[{ required: true, message: 'Please input your password!' }]}
-      >
-        <Input.Password />
-      </Form.Item>
-      <Form.Item name="remember" valuePropName="checked">
-        <Checkbox>Remember me</Checkbox>
-      </Form.Item>
-      <Form.Item>
-        <Button type="default" htmlType="submit">
+    <Form
+      form={form}
+      style={{ width: 600 }}
+      initialValues={{ name: 'admin' }}
+      autoComplete="off"
+      onValuesChange={(v, vs) => {
+        console.log(v, vs)
+      }}
+      onSubmit={v => {
+        console.log(v)
+      }}
+    >
+      <FormItem label="Username" field="name" rules={[{ required: true }]}>
+        <Input placeholder="please enter your username" />
+      </FormItem>
+      <FormItem label="Age" field="age" rules={[{ required: true, type: 'number', min: 0, max: 99 }]}>
+        <InputNumber placeholder="please enter your age" />
+      </FormItem>
+      <FormItem wrapperCol={{ offset: 5 }}>
+        <Button type="primary" htmlType="submit" style={{ marginRight: 24 }}>
           Submit
         </Button>
-      </Form.Item>
+        <Button
+          style={{ marginRight: 24 }}
+          onClick={() => {
+            form.resetFields()
+          }}
+        >
+          Reset
+        </Button>
+        <Button
+          type="text"
+          onClick={() => {
+            debugger
+            form.setFieldsValue({
+              name: 'admin',
+              age: 11
+            })
+          }}
+        >
+          Fill Form
+        </Button>
+
+        <Button
+          type="text"
+          onClick={() => {
+            console.log(form.getFieldsValue())
+          }}
+        >
+          获取所有值
+        </Button>
+      </FormItem>
     </Form>
   )
 }
 
-export default FormDe
+export default App
