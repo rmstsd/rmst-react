f31858ace4d8d70adc03d2033b968a5c2282b774
春雷-李春雷 <chunlei.lcl@naturobot.com>
2023-07-06 19:47:13 +0800

    v

--- src/views/Misc.tsx
+++ src/views/Misc.tsx
@@ -1,6 +1,7 @@
 import { forwardRef, useEffect, useImperativeHandle, useRef, useState } from 'react'
 import VirtualList from '@/components/virtual-scroll-list'
-import { Form, Button, Input, Tag, Checkbox, TimePicker } from '@arco-design/web-react'
+import { Form, Button, Input, Tag, Checkbox, TimePicker, Radio } from '@arco-design/web-react'
+import VirtualTinyList from 'react-tiny-virtual-list'
 
 const TOTAL_COUNT = 1000
 
@@ -15,9 +16,20 @@ while (count--) {
   })
 }
 
+const data = [...Array(100)]
+
 const Misc = () => {
   return (
     <div>
+      <VirtualTinyList
+        width={300}
+        height={300}
+        itemCount={data.length}
+        itemSize={30}
+        style={{ border: '1px solid' }}
+        renderItem={Item}
+      />
+      {/*   
       <VirtualList
         className="list"
         style={{ height: 600, overflow: 'auto', border: '2px solid #333' }}
@@ -28,7 +40,41 @@ const Misc = () => {
         estimateSize={30}
         // header={<div style={{ height: 100 }}>header</div>}
         // footer={<div style={{ height: 80 }}>footer</div>}
-      />
+      /> */}
+    </div>
+  )
+}
+
+const Item = ({ index, style }) => {
+  let date = Date.now()
+
+  return (
+    <div key={index} style={style}>
+      Row: #{index}
+      <br />
+      <Form autoComplete="off">
+        <Form.Item label="Layout">
+          <Radio.Group type="button" name="layout">
+            <Radio value="horizontal">horizontal</Radio>
+            <Radio value="vertical">vertical</Radio>
+            <Radio value="inline">inline</Radio>
+          </Radio.Group>
+        </Form.Item>
+        <Form.Item label="Username" field="username" rules={[{ required: true }]}>
+          <Input style={{ width: 270 }} placeholder="please enter your name" />
+        </Form.Item>
+        <Form.Item label="Post">
+          <Input style={{ width: 270 }} placeholder="please enter your post" />
+        </Form.Item>
+        <Form.Item>
+          <Checkbox>I have read the manual</Checkbox>
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   )
 }
