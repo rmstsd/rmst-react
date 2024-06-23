1e555b95553d3f26b4de55c184a59f755e3f3ed6
rmst <rmstsd@qq.com>
2024-06-23 16:13:38 +0800

    f

--- src/views/ReactflowDemo/index.tsx
+++ src/views/ReactflowDemo/index.tsx
@@ -1,85 +1,42 @@
-import React, { useCallback } from 'react'
-import ReactFlow, { addEdge, ConnectionLineType, Panel, useNodesState, useEdgesState } from 'reactflow'
-import dagre from 'dagre'
+import { useEffect } from 'react'
 
-import { initialNodes, initialEdges } from './nodes-edges.js'
-
-import 'reactflow/dist/style.css'
-
-const dagreGraph = new dagre.graphlib.Graph()
-dagreGraph.setDefaultEdgeLabel(() => ({}))
-
-const nodeWidth = 172
-const nodeHeight = 36
-
-const getLayoutedElements = (nodes, edges, direction = 'TB') => {
-  const isHorizontal = direction === 'LR'
-  dagreGraph.setGraph({ rankdir: direction })
-
-  nodes.forEach(node => {
-    dagreGraph.setNode(node.id, { width: nodeWidth, height: nodeHeight })
-  })
-
-  edges.forEach(edge => {
-    dagreGraph.setEdge(edge.source, edge.target)
-  })
-
-  dagre.layout(dagreGraph)
-
-  nodes.forEach(node => {
-    const nodeWithPosition = dagreGraph.node(node.id)
-    node.targetPosition = isHorizontal ? 'left' : 'top'
-    node.sourcePosition = isHorizontal ? 'right' : 'bottom'
-
-    // We are shifting the dagre node position (anchor=center center) to the top left
-    // so it matches the React Flow node anchor point (top left).
-    node.position = {
-      x: nodeWithPosition.x - nodeWidth / 2,
-      y: nodeWithPosition.y - nodeHeight / 2
+function onKeydownOnce(el: HTMLElement) {
+  const map = {}
+  el.addEventListener('keydown', evt => {
+    if (evt.key) {
     }
-
-    return node
   })
-
-  return { nodes, edges }
+  el.addEventListener('keyup', evt => {})
 }
 
-const { nodes: layoutedNodes, edges: layoutedEdges } = getLayoutedElements(initialNodes, initialEdges)
+export default function Km() {
+  useEffect(() => {
+    let spaceDown = false
 
-const ReactflowDemo = () => {
-  const [nodes, setNodes, onNodesChange] = useNodesState(layoutedNodes)
-  const [edges, setEdges, onEdgesChange] = useEdgesState(layoutedEdges)
+    const map = {}
 
-  const onConnect = useCallback(
-    params => setEdges(eds => addEdge({ ...params, type: ConnectionLineType.SmoothStep, animated: true }, eds)),
-    []
-  )
-  const onLayout = useCallback(
-    direction => {
-      const { nodes: layoutedNodes, edges: layoutedEdges } = getLayoutedElements(nodes, edges, direction)
+    window.addEventListener('keydown', evt => {
+      // if (evt.key == ' ') {
+      if (!spaceDown) {
+        spaceDown = true
+        console.log(11)
+      }
+      // }
+    })
 
-      setNodes([...layoutedNodes])
-      setEdges([...layoutedEdges])
-    },
-    [nodes, edges]
-  )
+    window.addEventListener('keyup', evt => {
+      // if (evt.key == ' ') {
+      if (spaceDown) {
+        spaceDown = false
+      }
+      // }
+    })
 
-  return (
-    <ReactFlow
-      nodes={nodes}
-      edges={edges}
-      onNodesChange={onNodesChange}
-      onEdgesChange={onEdgesChange}
-      onConnect={onConnect}
-      connectionLineType={ConnectionLineType.SmoothStep}
-      fitView
-    >
-      <Panel position="top-right">
-        <button onClick={() => onLayout('TB')}>vertical layout</button>
-        <button onClick={() => onLayout('LR')}>horizontal layout</button>
-      </Panel>
-    </ReactFlow>
-  )
+    window.addEventListener('pointerdown', evt => {
+      if (spaceDown) {
+        console.log(evt)
+      }
+    })
+  }, [])
+  return <div>index</div>
 }
-
-export default ReactflowDemo
