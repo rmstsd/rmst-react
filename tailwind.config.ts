594ec5ed94adc40a3fe57496976e6821464df4e9
rmst <rmstsd@qq.com>
2024-08-04 20:45:40 +0800

    r

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,63 @@
 import type { Config } from 'tailwindcss'
 import plugin from 'tailwindcss/plugin'
 
+const spacingList = [
+  '0',
+  '0.5',
+  '1',
+  '1.5',
+  '2',
+  '2.5',
+  '3',
+  '3.5',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '10',
+  '11',
+  '12',
+  '14',
+  '16',
+  '20',
+  '24',
+  '28',
+  '32',
+  '36',
+  '40',
+  '44',
+  '48',
+  '52',
+  '56',
+  '60',
+  '64',
+  '72',
+  '80',
+  '96'
+]
+
+const spacing = spacingList.reduce((acc, item) => Object.assign(acc, { [`${item}`]: `${item}px` }), {})
+
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
+    spacing,
+    'f-size': {
+      12: '12px',
+      14: '14px',
+      16: '16px',
+      18: '18px',
+      24: '24px',
+      32: '32px'
+    },
     extend: {}
   },
   plugins: [
-    plugin(function (p) {
-      const { addComponents, theme } = p
+    plugin(p => {
+      const { addComponents, addBase, addUtilities, addVariant, theme, matchUtilities } = p
+
       addComponents({
         '.flex-center': {
           display: 'flex',
@@ -16,6 +65,15 @@ export default {
           alignItems: 'center'
         }
       })
+
+      matchUtilities(
+        {
+          'f-size': value => ({ 'font-size': value })
+        },
+        {
+          values: theme('f-size')
+        }
+      )
     })
   ],
   corePlugins: {
