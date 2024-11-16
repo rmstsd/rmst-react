1dc8b72e259f6f6253468031f4c27eec662a3514
rmst <rmstsd@qq.com>
2024-11-16 11:00:58 +0800

    cccc

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,36 @@ import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from './App'
 
-import '@arco-design/web-react/dist/css/arco.css'
-import './app.less'
+import ElementQueries from 'css-element-queries/src/ElementQueries'
+ElementQueries.listen()
+
+function App2() {
+  const [b, sb] = useState(false)
+
+  return (
+    <>
+      <button onClick={() => sb(!b)}>set</button>
+
+      {b && (
+        <div className="widget-name">
+          <h2>Element responsiveness FTW!</h2>
+        </div>
+      )}
+    </>
+  )
+}
+
+// import '@arco-design/web-react/dist/css/arco.css'
+// import './app.less'
+import { RectRender, registerUI } from 'leafer-ui'
+
+import './test.css'
+import { useState } from 'react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <HashRouter>
     <ConfigProvider componentConfig={{ Button: { type: 'primary' } }}>
-      <App />
+      <App2 />
     </ConfigProvider>
   </HashRouter>
 )
--- src/views/Immer/index.tsx
+++ src/views/Immer/index.tsx
@@ -1,42 +1,52 @@
-import { Select } from '@arco-design/web-react'
-import { useState, forwardRef, useImperativeHandle, useEffect } from 'react'
+import { Input } from '@arco-design/web-react'
+import { RefInputType } from '@arco-design/web-react/es/Input'
+import { toJS } from 'mobx'
+import { observer, useLocalObservable } from 'mobx-react-lite'
+import { useRef, useState } from 'react'
 
-export default function Tt() {
-  const [flag, setFlag] = useState(false)
-  const [value, setValue] = useState('')
-
-  console.log('render')
-
-  const [options, setOptions] = useState(() => {
-    return Array.from({ length: 30 }, (_, index) => ({ label: `Option ${index + 1}`, value: index }))
-  })
-
-  return (
-    <>
-      <button onClick={() => setFlag(!flag)}>btn</button>
-
-      <Select
-        value={value}
-        options={options}
-        mode="multiple"
-        onChange={value => {
-          setValue(value)
-
-          setOptions([])
-
-          setTimeout(() => {
-            setOptions(Array.from({ length: 30 }, (_, index) => ({ label: `Option ${index + 1}`, value: index })))
-          }, 500)
-        }}
-      ></Select>
-    </>
-  )
+interface User {
+  name: string
+  gender: string
 }
 
-let Child = forwardRef(function Child(props, ref) {
-  useImperativeHandle(ref, () => ({
-    ip: 1
-  }))
+export default observer(function ImDemo() {
+  const store = useLocalObservable(() => ({ name: '', gender: '', list: [] }))
 
-  return <div></div>
+  const nameRef = useRef<RefInputType>(null)
+
+  return (
+    <div>
+      <>FocusDemo</>
+
+      <hr />
+
+      <div style={{ display: 'flex', gap: '10px' }}>
+        <div>
+          <span>name:</span>
+          <Input autoFocus value={store.name} onChange={v => (store.name = v)} ref={nameRef} />
+        </div>
+        <div>
+          <span>gender:</span>
+          <Input
+            value={store.gender}
+            onChange={v => (store.gender = v)}
+            onPressEnter={event => {
+              console.log(toJS(store))
+
+              store.list.push({ name: store.name, gender: store.gender })
+              store.name = ''
+              store.gender = ''
+              nameRef.current.focus()
+            }}
+          />
+        </div>
+      </div>
+      <hr />
+      {store.list.map((item, index) => (
+        <div key={index}>
+          {item.name}-{item.gender}
+        </div>
+      ))}
+    </div>
+  )
 })
