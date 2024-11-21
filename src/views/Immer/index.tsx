bb1598957e9997dda7eeef7707c6466648b46198
rmst <rmstsd@qq.com>
2024-11-21 10:56:28 +0800

    code

--- src/views/Immer/index.tsx
+++ src/views/Immer/index.tsx
@@ -2,7 +2,7 @@ import { Input } from '@arco-design/web-react'
 import { RefInputType } from '@arco-design/web-react/es/Input'
 import { toJS } from 'mobx'
 import { observer, useLocalObservable } from 'mobx-react-lite'
-import { useEffect } from 'react'
+import { forwardRef, useEffect, useImperativeHandle } from 'react'
 import { useRef, useState } from 'react'
 import { Html5Qrcode, Html5QrcodeScanner } from 'html5-qrcode'
 import { noop } from 'es-toolkit'
@@ -19,7 +19,6 @@ export default observer(function ImDemo() {
 
   const qrCodeSuccessCallback = (decodedText, decodedResult) => {
     dddd(JSON.stringify(decodedResult, null, 2))
-
     setvv(decodedText)
 
     html5QrCodem.stop()
@@ -30,20 +29,23 @@ export default observer(function ImDemo() {
   const config = { fps: 10, qrbox: { width: 250, height: 250 } }
 
   return (
-    <div>
+    <div className="mt-20">
       <button
         onClick={() => {
+          dddd('')
+          setvv('')
+
           html5QrCodem.start({ facingMode: 'environment' }, config, qrCodeSuccessCallback).catch(err => err)
         }}
       >
-        scan
+        开始扫码
       </button>
 
-      <button onClick={() => html5QrCodem.stop()}>cancel</button>
+      <button onClick={() => html5QrCodem.stop()}>停止扫码</button>
 
       <input type="text" value={vv} onChange={noop} />
 
-      <div id="reader"></div>
+      <div id="reader" className="w-100 h-100"></div>
 
       <pre>{dd}</pre>
     </div>
