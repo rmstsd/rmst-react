33d68ed15939de907869a89254f1218b2778de3e
rmst <rmstsd@qq.com>
2025-01-08 09:08:26 +0800

    no message

--- src/views/RmstSd/Rmstsd.tsx
+++ src/views/RmstSd/Rmstsd.tsx
@@ -1,66 +1,88 @@
-import { DndContext, MouseSensor, useDraggable, useSensor, useSensors } from '@dnd-kit/core'
-import { CSS } from '@dnd-kit/utilities'
-import { useEffect } from 'react'
+import { useInterval } from 'ahooks'
+import { PointerEvent } from 'react'
+
+let isDragging = false
+
+document.addEventListener(
+  'click',
+  evt => {
+    if (isDragging) {
+      evt.preventDefault()
+      evt.stopPropagation()
+    }
+  },
+  { capture: true }
+)
+document.addEventListener(
+  'change',
+  evt => {
+    // if (isDragging) {
+    //   evt.stopPropagation()
+    // }
+    // evt.stopPropagation()
+  },
+  { capture: true }
+)
 
 export default function Rmstsd() {
-  const sensors = useSensors(
-    useSensor(MouseSensor, {
-      activationConstraint: { distance: 5 }
-    })
-  )
+  const onPointerDown = (downEvt: PointerEvent) => {
+    downEvt.preventDefault()
 
-  return (
-    <div>
-      <DndContext sensors={sensors}>
-        <Draggable />
-      </DndContext>
-    </div>
-  )
-}
+    const ab = new AbortController()
 
-function Draggable() {
-  const { attributes, listeners, setNodeRef, transform } = useDraggable({
-    id: 'draggable'
-  })
-  const style = {
-    transform: CSS.Translate.toString(transform)
+    document.addEventListener(
+      'pointermove',
+      evt => {
+        const dis = Math.sqrt((downEvt.clientX - evt.clientX) ** 2 + (downEvt.clientY - evt.clientY) ** 2)
+        if (!isDragging) {
+          if (dis > 10) {
+            isDragging = true
+
+            console.log('drag start')
+
+            // drag start
+          }
+          return
+        }
+
+        // move
+      },
+      {
+        signal: ab.signal
+      }
+    )
+    document.addEventListener(
+      'pointerup',
+      evt => {
+        setTimeout(() => {
+          isDragging = false
+        })
+
+        ab.abort()
+      },
+      {
+        signal: ab.signal
+      }
+    )
   }
 
-  useEffect(() => {
-    document.addEventListener(
-      'change',
-      evt => {
-        console.log('change capture', evt.target)
-        evt.stopPropagation()
-      },
-      { capture: true }
-    )
-  }, [])
+  useInterval(() => {
+    // console.log(document.querySelector('input').checked)
+  }, 1000)
 
   return (
-    <label
-      className="inline-block"
-      {...attributes}
-      {...listeners}
-      ref={setNodeRef}
-      style={style}
-      onClick={evt => {
-        console.log('click', evt.target)
-      }}
-    >
+    <label className="inline-block" onPointerDown={onPointerDown}>
       <input
+        style={{ zoom: 4 }}
         type="checkbox"
-        onChange={evt => {
-          console.log('change', evt.target.checked)
-        }}
-        onClick={() => {
+        onClick={evt => {
           console.log('input click ')
         }}
-        onClickCapture={() => {
-          console.log('input click capture')
+        onChange={evt => {
+          console.log('input change')
         }}
       />
-      <span>aa</span>
+      <span>发过火华工科技电风扇</span>
     </label>
   )
 }
