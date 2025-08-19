92f8647c954d3d121439d6630f5c04245936af26
rmst <rmstsd@qq.com>
2025-08-19 21:12:31 +0800

    aa

--- src/views/_RmstSd/Rmstsd.tsx
+++ src/views/_RmstSd/Rmstsd.tsx
@@ -1,54 +1,6 @@
 import { InputNumber, Select } from '@arco-design/web-react'
-import { $, sharex } from 'helux'
 import React, { useState } from 'react'
 
-const {
-  reactive: outer,
-  useReactive,
-  flush
-} = sharex({
-  a: 1,
-  b: { b1: { b2: 1, ok: true } },
-
-  user: {
-    love: 'asdas'
-  }
-})
-
-export default function Aa() {
-  return (
-    <div className="h-[500px]">
-      <div></div>
-    </div>
-  )
-}
-
-function Rmstsd() {
-  const [reactive] = useReactive()
-
-  const { b, user } = reactive
-
-  return (
-    <div>
-      <button onClick={() => outer.a++}>{reactive.a}</button>
-
-      <h1
-        onClick={() => {
-          reactive.a++
-        }}
-      >
-        {b.b1.b2}
-      </h1>
-
-      <input
-        type="text"
-        className="border"
-        value={user.love}
-        onChange={evt => {
-          user.love = evt.target.value
-          flush()
-        }}
-      />
-    </div>
-  )
+export default function Rmstsd() {
+  return <div>Rmstsd</div>
 }
